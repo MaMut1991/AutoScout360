fae01e80da6f77d8270d0e08ab3e7424d297a1dc
Mario <doctorbeat01@gmail.com>
2024-11-15 11:17:31 +0100

    Added new file

---
+++ analysis.py
@@ -0,0 +1,1 @@
+# In this file, you can find some further analysis that may be helpful for finding a car.
---
+++ model.py
@@ -0,0 +1,2 @@
+# In this file everything is around Machine Learning: find / train / tune and evaluate algorithm
+
