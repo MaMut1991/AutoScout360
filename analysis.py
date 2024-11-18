25412e04217efe4b5911a7c5f3fd2776a2fd1acc
Mario <doctorbeat01@gmail.com>
2024-11-18 16:52:10 +0100

    Completed filters and started session_state for reset-button

--- analysis.py
+++ analysis.py
@@ -5,10 +5,27 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import streamlit as st
 
 data = pd.read_csv('cleaned_dataframe_for_analysis.csv')
 
-# Get lists of unique feature values for multiselect options in main
+# Some Transformings:
+# Drop first column
+data = data.drop(data.columns[0], axis=1) 
+# Remove commas as thousand separators in year values
+data['year_registration'] = data['year_registration'].astype(str)
+data['year_registration'] = data['year_registration'].str.replace(',','')
+data['year_registration'] = data['year_registration'].astype(int) 
+# Cast 'hp' into int
+data['hp'] = data['hp'].astype('int')
+# New column order
+new_column_order = ['car_brand', 'model', 'propulsion', 'gear', 'hp', 'mileage', 'year_registration', 'offerType', 'price']
+data = data[new_column_order]
+
+
+
+
+# Get lists of unique feature values for multiselect options in main for filters
 def get_multiselect_options():
 
     options_mileage = data['mileage'].unique()
@@ -20,4 +37,40 @@ def get_multiselect_options():
     options_year = data['year_registration'].unique()
 
     return options_mileage, options_brand,options_model, options_fuel, options_gear, options_hp, options_year
+
+# Provide data 
+def provide_data_find(input_mileage_find,input_brand_find,input_model_find,input_fuel_find,input_gear_find,input_hp_find,input_year_find):
+
+    # Filter
+    # mileage
+    filtered_data = data[(data['mileage'] >= input_mileage_find[0]) & (data['mileage'] < input_mileage_find[1])]
+    # car brand
+    if input_brand_find:
+        filtered_data = filtered_data[filtered_data['car_brand'].isin(input_brand_find)]
+    # model
+    if input_model_find:
+        filtered_data = filtered_data[filtered_data['model'].isin(input_model_find)]
+    # Fuel
+    if input_fuel_find:
+        filtered_data = filtered_data[filtered_data['propulsion'].isin(input_fuel_find)]
+    # Gear
+    if input_gear_find:
+        filtered_data = filtered_data[filtered_data['gear'].isin(input_gear_find)]
+    # Hp
+    if input_hp_find:
+        filtered_data = filtered_data[(filtered_data['hp'] >= input_hp_find[0]) & (filtered_data['hp'] < input_hp_find[1])]
+    # Year of Registration
+    if input_year_find:
+        filtered_data = filtered_data[(filtered_data['year_registration'] >= input_year_find[0]) & (filtered_data['year_registration'] < input_year_find[1])]
+    
+
+    # Drop index
+    filtered_data = filtered_data.reset_index(drop=True)
+
+    # Prints
+    st.write("Filtered Data:")
+    st.table(data=filtered_data)
+
+
+
 
--- main.py
+++ main.py
@@ -6,6 +6,10 @@
 import streamlit as st
 
 from analysis import get_multiselect_options
+from analysis import provide_data_find
+
+# Layout
+st.set_page_config(layout="wide")
 
 # Title Streamlit 
 st.header('AutoScout360 - With some clever insights to your new car!')
@@ -13,29 +17,53 @@ st.header('AutoScout360 - With some clever insights to your new car!')
 # Sidebar
 
 # Find car
+# Create filters
 options_mileage, options_brand,options_model, options_fuel, options_gear, options_hp, options_year = get_multiselect_options()
 
+# Set default values
+default_values = {
+    'find_car_mileage':(options_mileage.min(),options_mileage.max()),
+    'find_car_brand':[],
+    'find_car_model':[],
+    'find_car_fuel':[],
+    'find_car_gear':[],
+    'find_car_hp':(options_hp.min(), options_hp.max()),
+    'find_car_year':(options_year.min(), options_hp.max())
+}
+
 st.sidebar.markdown('# FIND your new car:')
-input_mileage_find = st.sidebar.slider(label='Choose range for mileage:',min_value=options_mileage.min(),max_value=options_mileage.max(),value=(options_mileage.min(),options_mileage.max()), key='find_car_mileage')
+input_mileage_find = st.sidebar.slider(label='Mileage:',min_value=options_mileage.min(),max_value=options_mileage.max(),value=(options_mileage.min(),options_mileage.max()), key='find_car_mileage')
 input_brand_find = st.sidebar.multiselect(label='Car Brand:',options=options_brand, key='find_car_brand')
 input_model_find = st.sidebar.multiselect(label='Model:', options=options_model, key='find_car_model')
-input_fuel_find = st.sidebar.multiselect(label='Fuel:',options=options_fuel,key='find_car_fuel')
+input_fuel_find = st.sidebar.multiselect(label='Propullsion:',options=options_fuel,key='find_car_fuel')
 input_gear_find = st.sidebar.multiselect(label='Gear:',options=options_gear,key='find_car_gear')
-input_hp_find = st.sidebar.multiselect(label='Horsepower:',options=options_hp,key='find_car_hp')
-input_year_find = st.sidebar.slider(label='Choose range for Year of Registration:', min_value=options_year.min(), max_value=options_year.max(), value=(options_year.min(),options_year.max()),key='find_car_year')
+input_hp_find = st.sidebar.slider(label='Horsepower:',min_value=options_hp.min(),max_value=options_hp.max(), value=(options_hp.min(), options_hp.max()),key='find_car_hp')
+input_year_find = st.sidebar.slider(label='Year of Registration:', min_value=options_year.min(), max_value=options_year.max(), value=(options_year.min(),options_year.max()),key='find_car_year')
 
 col1,col2 = st.sidebar.columns(2)
 button_find_car = col1.button(label='Find car',key='find_car_button')
 button_find_car_reset = col2.button(label='Reset', key='find_car_reset_button') 
 
+# Provide data
+if button_find_car:
+    provide_data_find(input_mileage_find,input_brand_find,input_model_find,input_fuel_find,input_gear_find,input_hp_find,input_year_find)
+
+# Reset button logic
+if button_find_car_reset:
+    for key,value in default_values.items():
+        st.session_state[key] = value
+
+
 st.sidebar.markdown('---')  # Spatial separation 
 
+
 # Sell car
+# Filters
 st.sidebar.markdown('# SELL your old car:')
 input_mileage_sell = st.sidebar.number_input(label='Enter Mileage:',placeholder='None',key='sell_car_mileage', step=1)
 input_brand_sell = st.sidebar.multiselect(label='Car Brand:',options=options_brand,key='sell_car_brand')
 input_model_sell = st.sidebar.multiselect(label='Model:',options=options_model,key='sell_car_model')
-input_fuel_sell = st.sidebar.multiselect(label='Fuel:',options=options_fuel,key='sell_car_fuel')
+input_fuel_sell = st.sidebar.multiselect(label='Propulsion:',options=options_fuel,key='sell_car_fuel')
 input_gear_sell = st.sidebar.multiselect(label='Gear:',options=options_gear,key='sell_car_gear')
 input_hp_sell = st.sidebar.number_input(label='Enter Horsepower:',key='sell_car_hp',step=1)
 input_year_sell = st.sidebar.number_input(label='Enter Year of Registration',key='sell_car_year', step=1)
